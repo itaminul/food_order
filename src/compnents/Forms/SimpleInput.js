5d89d67e1c156e23f555357f837ecfbe40262a70
itaminul@gmail.com <itaminul@gmail.com>
2022-07-27 00:40:12 +0600

    form validation

--- src/compnents/Forms/SimpleInput.js
+++ src/compnents/Forms/SimpleInput.js
@@ -1,48 +1,52 @@
 import { isValidInputTimeValue } from "@testing-library/user-event/dist/utils";
 import { useEffect, useState } from "react";
-
+import './SimpleInput.css'
 const SimpleInput = () => {
     const [enteredName, setEnteredName] = useState('');
-    const [enteredNameIsValid, setEnteredNameIsValied] = useState(true)
     const [enteredNameTouched, setEntredNameTouched] = useState(false)
 
-    useEffect(() => {
-        if(enteredNameIsValid) {
-            console.log('Name input is valie!');
-        }
-    }, [enteredNameIsValid])
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
 
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value);
     }
-    const formSubmissionHandler = event => {
-        event.preventDefault();
+
+    const nameInputBlurHandler = event => {
         setEntredNameTouched(true);
 
-        if(enteredName.trim() === '') {
-            setEnteredNameIsValied(false)
+
+    }
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        setEntredNameTouched(true);
+
+        if(!enteredNameIsValid) {
             return;
         }
-        setEnteredNameIsValied(true)
 
-
-        console.log(enteredName);
         setEnteredName('');
+        setEntredNameTouched(false);
     }
 
-    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
-    const nameINputClasses = nameInputIsInvalid ? 'className="bg-indigo-500"' : 'className="bg-indigo-500"';
+    const nameINputClasses = nameInputIsInvalid 
+    ? 'form-control invalid' 
+    : 'form-control';
 
 
 
     return(
         <div>
            <form onSubmit={formSubmissionHandler}>
-               <div>
+               <div className={nameINputClasses}>
                    <label htmlFor="">Name</label>
-                   <input type="name" className="nameINputClasses bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" 
-                   id="name" onChange={nameInputChangeHandler}
+                   <input type="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" 
+                   id="name" 
+                   onChange={nameInputChangeHandler}
+                   onBlur={nameInputBlurHandler}
                    value={enteredName}
                    />
                    {nameInputIsInvalid && <p className="text-red-500">Name must not be empty</p>}
